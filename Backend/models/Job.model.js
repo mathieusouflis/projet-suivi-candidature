6cc2e235ac2f490705d9fc457ca76c746f342aff
loaiattar <loaiattar@gmail.com>
2025-03-25 12:48:34 +0100

    feat: :rocket: Creating a Job model

---
+++ Backend/models/Job.model.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+// define Job schema
+const jobSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  company: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  salary: {
+    type: String,  // normally we have to use Number as a type but i used String for avoiding managing ranges, we can change it later if we want.
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+//create a job model: 
+const Job = mongoose.model('Job', jobSchema);
+
+export default Job; 
