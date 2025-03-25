fc8bc85a0bae6a3ca3193bb2640fe7abb08f4dbd
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-03-25 15:12:36 +0100

    fix: :art: Config frontend key

--- Frontend/src/config/config.js
+++ Frontend/src/config/config.js
@@ -3,7 +3,7 @@ export default config = {
         baseUrl: import.meta.env.API_BASE_URL || "http://localhost:3000",
         timeout: import.meta.env.API_TIMEOUT || 5000,
     },
-    api: {
+    frontend: {
         MODE: import.meta.env.MODE || "development",
     }
 }
