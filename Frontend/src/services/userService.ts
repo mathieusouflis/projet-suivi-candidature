5ed9faec916c0acb2a2371c151024543831f6ed8
Mathieu <39670015+mathieusouflis@users.noreply.github.com>
2025-03-25 15:46:12 +0100

    refactor: :fire: Typescript Migration 2.0

--- Frontend/src/services/userService.ts
+++ Frontend/src/services/userService.ts
@@ -1,17 +1,18 @@
+import { User } from "@/types/user";
 import { callApi } from "./apiService";
 
-export const createUser = async (user) => {
+export const createUser = async (user: User) => {
     return await callApi('/users', 'POST', user);
 }
 
-export const getUserById = async (id) => {
+export const getUserById = async (id: number) => {
     return await callApi(`/users/${id}`, 'GET');
 }
 
-export const updateUser = async (user) => {
+export const updateUser = async (user: User) => {
     return await callApi(`/users/${user.id}`, 'PUT', user);
 }
 
-export const deleteUser = async (id) => {
+export const deleteUser = async (id: number) => {
     return await callApi(`/users/${id}`, 'DELETE');
 }
---
+++ Frontend/src/types/contact.ts
@@ -0,0 +1,11 @@
+export interface Contact {
+    id: number;
+    name: string;
+    lastNae: string;
+    role: string;
+    tel: string;
+    email: string;
+    userId: number;
+    creationDate: string;
+    lastUpdatedDate: string;
+}
